ceeacb16d3ced0002e3ce0ea9800a9c02cf780d0
Bohdan <154711952+7GitGuru@users.noreply.github.com>
2024-02-16 20:06:09 +0200

    ⚙️Changed icons

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,17 +1,11 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgFigma } from "react-icons/cg";
-import { DiDocker, DiFirebase, DiGit, DiGithub, DiMongodb, DiMysql } from "react-icons/di";
-import {
-  SiVercel,
-} from "react-icons/si";
+import { DiDocker, DiFirebase, DiGit, DiGithubBadge, DiMongodb, DiMysql } from "react-icons/di";
+// import { SiVercel } from "react-icons/si";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <CgFigma />
-      </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
       </Col>
@@ -22,14 +16,14 @@ function Toolstack() {
         <DiGit />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiGithub />
+        <DiGithubBadge />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiDocker />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      {/* <Col xs={4} md={2} className="tech-icons">
         <SiVercel />
-      </Col>
+      </Col> */}
       <Col xs={4} md={2} className="tech-icons">
         <DiFirebase />
       </Col>
